2c0fe3dfe369e35885b3c3f851a15b16ae55c9f4
Nico Orru <nigu.orru@gmail.com>
2018-09-28 00:17:38 +0100

    Gfx implementation WIP

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,66 @@ extern crate gtk;
 extern crate libc;
 extern crate shared_library;
 
-use std::ptr;
-
 use gfx::format::Formatted;
 use gfx::memory::Typed;
-use gfx::{format, handle, texture};
-use gfx_device_gl as device_gl;
-use gfx_device_gl::Resources as R;
+use gfx::{format, handle, texture, Device, Encoder, Factory};
+use gfx_device_gl;
 use gtk::traits::*;
 use gtk::Inhibit;
 use gtk::Window;
+use std::cell::RefCell;
+use std::ptr;
+use std::rc::Rc;
 
 pub type ScreenColorChannels = gfx::format::R8_G8_B8_A8;
 // Srgba8 broken on Linux
 pub type ScreenColorFormat = (ScreenColorChannels, gfx::format::Unorm);
 // Srgba8 broken on Linux
 pub type ScreenDepthFormat = gfx::format::Depth;
+
+type Rgba = [f32; 4];
+type Depth = f32;
+
+struct GfxContext<D, F, C>
+where
+	D: gfx::Device,
+	F: gfx::Factory<D::Resources>,
+	C: gfx::CommandBuffer<D::Resources>,
+{
+	device: D,
+	factory: F,
+	encoder: Encoder<D::Resources, C>,
+	frame_buffer: gfx::handle::RenderTargetView<D::Resources, ScreenColorFormat>,
+	depth_buffer: gfx::handle::DepthStencilView<D::Resources, ScreenDepthFormat>,
+	background_color: Rgba,
+	background_depth: Depth,
+}
+
+type GlDevice = gfx_device_gl::Device;
+type GlFactory = gfx_device_gl::Factory;
+type GlResources = <GlDevice as gfx::Device>::Resources;
+type GlCommandBuffer = gfx_device_gl::CommandBuffer;
+
+pub trait Renderer {
+	fn render(&mut self);
+}
+
+impl<D, F, C> Renderer for GfxContext<D, F, C>
+where
+	D: gfx::Device,
+	F: Factory<D::Resources>,
+	C: gfx::CommandBuffer<D::Resources>,
+{
+	fn render(&mut self) {
+		self.encoder
+			.clear(&self.frame_buffer, self.background_color);
+		self.encoder
+			.clear_depth(&self.depth_buffer, self.background_depth);
+		//self.encoder.flush(&mut self.device);
+	}
+}
+
+type GlGfxContext = GfxContext<GlDevice, GlFactory, GlCommandBuffer>;
 
 pub fn main() {
 	if gtk::init().is_err() {
@@ -64,6 +108,8 @@ pub fn main() {
 		Inhibit(false)
 	});
 
+	let gfx_context: Rc<RefCell<Option<GlGfxContext>>> = Rc::new(RefCell::new(None));
+
 	glarea.connect_realize(|widget| {
 		if widget.get_realized() {
 			widget.make_current();
@@ -73,14 +119,19 @@ pub fn main() {
 
 		// create the main color/depth targets
 		let ptr = unsafe { gl::GetString(gl::VENDOR) };
-		let dim = (allocation.width as u16, allocation.height as u16, 1, gfx::texture::AaMode::Single);
+		let dim = (
+			allocation.width as u16,
+			allocation.height as u16,
+			1,
+			gfx::texture::AaMode::Single,
+		);
 
-		let (device, factory) = device_gl::create(get_proc_addr);
+		let (device, factory) = gfx_device_gl::create(get_proc_addr);
 
 		let color_format = ScreenColorFormat::get_format();
 		let depthstencil_format = ScreenDepthFormat::get_format();
 		let (color_view, ds_view) =
-			device_gl::create_main_targets_raw(dim, color_format.0, depthstencil_format.0);
+			gfx_device_gl::create_main_targets_raw(dim, color_format.0, depthstencil_format.0);
 	});
 
 	glarea.connect_render(|_, _| {
